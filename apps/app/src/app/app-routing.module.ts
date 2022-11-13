14ff5ab46a6962dc6f102455fd3b7dffb3581e1f
Ksawery <pawlikksawery@gmail.com>
2022-11-13 18:55:02 +0100

    Create new list

--- apps/app/src/app/app-routing.module.ts
+++ apps/app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import {NgModule} from "@angular/core";
-import {RouterModule} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductListComponentModule } from './components/product-list/product-list.component-module';
+import { ProductServiceModule } from './services/product.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'product', component: ProductListComponent }]), ProductListComponentModule, ProductServiceModule],
   providers: [],
   declarations: [],
   exports: []
--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -6,10 +6,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from "./app-routing.module";
 import {RouterModule} from "@angular/router";
+import {ProductListComponentModule} from "./components/product-list/product-list.component-module";
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MaterialModule, RouterModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MaterialModule, RouterModule, AppRoutingModule, ProductListComponentModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
